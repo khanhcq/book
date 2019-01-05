116520ee2c1ed68d71bfa8eabedcf90c08cfa7f4
Khanh Chu <khanh.chu@banvien.com>
2019-01-05 16:01:39 +0700

    clean up

--- src/main/java/com/karlchu/book/controller/MainController.java
+++ src/main/java/com/karlchu/book/controller/MainController.java
@@ -34,68 +34,7 @@ public class MainController {
     @ResponseBody
     @RequestMapping("/")
     public String home() {
-        String html = "";
-        html += "<ul>";
-        html += " <li><a href='/testInsert'>Test Insert</a></li>";
-        html += " <li><a href='/showAllEmployee'>Show All Employee</a></li>";
-        html += " <li><a href='/showFullNameLikeTom'>Show All 'Tom'</a></li>";
-        html += " <li><a href='/deleteAllEmployee'>Delete All Employee</a></li>";
-        html += "</ul>";
-        return html;
-    }
-
-    @ResponseBody
-    @RequestMapping("/testInsert")
-    public String testInsert() {
-        Employee employee = new Employee();
-
-        long id = this.employeeRepositoryCustom.getMaxEmpId() + 1;
-        int idx = (int) (id % NAMES.length);
-        String fullName = NAMES[idx] + " " + id;
-
-        employee.setId(id);
-        employee.setEmpNo("E" + id);
-        employee.setFullName(fullName);
-        employee.setHireDate(new Date());
-        this.employeeRepository.insert(employee);
-
-        return "Inserted: " + employee;
-    }
-
-    @ResponseBody
-    @RequestMapping("/showAllEmployee")
-    public String showAllEmployee() {
-
-        List<Employee> employees = this.employeeRepository.findAll();
-
-        String html = "";
-        for (Employee emp : employees) {
-            html += emp + "<br>";
-        }
-
-        return html;
-    }
-
-    @ResponseBody
-    @RequestMapping("/showFullNameLikeTom")
-    public String showFullNameLikeTom() {
-
-        List<Employee> employees = this.employeeRepository.findByFullNameLike("Tom");
-
-        String html = "";
-        for (Employee emp : employees) {
-            html += emp + "<br>";
-        }
-
-        return html;
-    }
-
-    @ResponseBody
-    @RequestMapping("/deleteAllEmployee")
-    public String deleteAllEmployee() {
-
-        this.employeeRepository.deleteAll();
-        return "Deleted!";
+        return "";
     }
 
     @RequestMapping(value = { "/employeeList" }, method = RequestMethod.GET)
