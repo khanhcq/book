d5760ca1a6aaf1ebc3b44cc6c7d5980b1d6436e7
Khanh Chu <khanh.chu@banvien.com>
2019-01-16 20:36:26 +0700

    yy crawler

--- src/main/java/com/karlchu/crawler/truyen_yy/TruyenYYCrawler.java
+++ src/main/java/com/karlchu/crawler/truyen_yy/TruyenYYCrawler.java
@@ -180,7 +180,7 @@ public class TruyenYYCrawler {
             filePath += ".vip";
         }
         Element mustLogin = document.select("#id_chap_content .inner.my-3").first();
-        if(mustLogin.text().trim().equals("Truyện này yêu cầu đăng nhập mới được xem chương. Đến trang Đăng Nhập hoặc Đăng Ký Tài Khoản.")){
+        if(mustLogin != null && mustLogin.text().trim().equals("Truyện này yêu cầu đăng nhập mới được xem chương. Đến trang Đăng Nhập hoặc Đăng Ký Tài Khoản.")){
             filePath += ".login";
         }
 
